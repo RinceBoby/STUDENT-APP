1e194b48cf32008caa98f52d10604115ba18c99f
RinceBoby <101865864+RinceBoby@users.noreply.github.com>
2022-07-04 16:23:48 +0530

    third

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -27,7 +27,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
-        builder: (context) => const HomeScreen(),
+        builder: (context) => HomeScreen(),
       ),
     );
   }
